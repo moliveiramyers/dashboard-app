9a511f4567901d1493c7198db787e490f7c17f45
moliveiramyers <moliveiramyers@byupathway.edu>
2026-07-19 16:01:27 +0100

    dynamic routes

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,17 +1,21 @@
 'use server';
 import z from "zod";
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+
 import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
-    amount: z.number(),
+    amount: z.coerce.number(),
     status: z.enum(['pending', 'paid']),
     date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({id:true,date:true})
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -21,8 +25,11 @@ export async function createInvoice(formData: FormData) {
     });
     // good practices to convert into cents instead of floating numbers
     const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];  
+    const date = new Date().toISOString().split('T')[0];
     await sql`
     INSERT INTO invoices(customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+
 }
