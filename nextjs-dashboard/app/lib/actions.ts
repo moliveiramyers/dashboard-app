edf3ef926d8d60f545b4a6caf1fa829c3ed3421a
moliveiramyers <moliveiramyers@byupathway.edu>
2026-07-20 15:18:31 +0100

    fix auth issue and add meta data icons

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -106,12 +106,14 @@ export async function deleteInvoice(id: string) {
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
   } catch (error) {
     console.error(error);
     return { message: 'Database Error: Failed to Delete Invoice.' };
 
   }
-  revalidatePath('/dashboard/invoices');
+  
 };
 export async function authenticate(prevState: string | undefined,
   formData: FormData,
